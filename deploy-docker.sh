79239a205da0bdbff765423623c98f18dd9890bd
Julio Viera <julio.viera@gmail.com>
2025-07-24 12:02:56 -0400

    Remove automatic push from docker build script
    - Remove --push flag to give more control over releases

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -11,4 +11,4 @@ if [ "$OPENVPN_PORT_UDP" == "off" ];then
     OPENVPN_PORT_UDP=
 fi
 
-docker buildx build --platform=linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm/v8,linux/386 . -t julman99/eatmemory:$VERSION  -t julman99/eatmemory:latest --push
+docker buildx build --platform=linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm/v8,linux/386 . -t julman99/eatmemory:$VERSION  -t julman99/eatmemory:latest 
