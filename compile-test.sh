1d487e01f6d49805810533d8a579d35a484a4600
Julio Viera <julio.viera@gmail.com>
2025-07-24 12:02:49 -0400

    Add cross-platform testing infrastructure
    - Add compile-test.sh for testing multiple GCC versions and architectures
    - Add dockcross.test.sh for testing 46+ cross-compilation targets
    - Add dockcross.images list of supported build environments

---
+++ compile-test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+function echo_test_start {
+  echo "***************************************************"
+  echo "*********** $1"
+  echo "***************************************************"
+}
+
+function test_gcc_linux {
+  local gcc_version="$1";
+  local platform="$2";
+  echo_test_start "linux gcc:$gcc_version $2"
+  docker run -it -v "$SCRIPT_DIR:/src" --rm --platform $platform gcc:$gcc_version bash -c "cd /src && make clean && make && output/eatmemory -?"
+}
+
+function test_all_gcc_linux {
+  local platforms="linux/amd64 linux/arm64 linux/amd64v8";
+  local gcc_versions="4 5 8 9 10 11 12 13";
+  for platform in $platforms
+  do
+    for gcc_version in $gcc_versions
+    do
+      test_gcc_linux $gcc_version $platform
+    done
+  done
+}
+
+test_all_gcc_linux
