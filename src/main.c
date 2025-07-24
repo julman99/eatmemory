a0840bec98fc08aedaf11e8c03cb8081a1bcf84e
Julio Viera <julio.viera@gmail.com>
2025-07-24 12:02:54 -0400

    Make percentage help text platform-conditional
    - Only show percentage option in help when platform supports memory stats
    - Prevents user confusion on unsupported platforms

--- src/main.c
+++ src/main.c
@@ -40,7 +40,9 @@ void print_help() {
     printf("#                # Bytes      example: 1024\n");
     printf("#M               # Megabytes  example: 15M\n");
     printf("#G               # Gigabytes  example: 2G\n");
+#ifndef SYSMEM_MODE_UNKOWN
     printf("#%%               # Percent    example: 50%%\n");
+#endif
     printf("\n");
     printf("Options:\n");
     printf("-t <seconds>     Exit after specified number of seconds.\n");
