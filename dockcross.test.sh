1d475f03a9eabca393062d9ad5157e581d3679d5
Julio Viera <julio.viera@gmail.com>
2025-07-24 13:36:21 -0400

    Dockercross test updates

--- dockcross.test.sh
+++ dockcross.test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Suppress Docker platform warnings on non linux/amd64 platforms
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 for image in $(cat $SCRIPT_DIR/dockcross.images); do
