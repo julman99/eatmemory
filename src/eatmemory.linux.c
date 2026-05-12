6b18d50853a856129032eee2f4380c0c6f8d4f48
Julio Viera <julio.viera@gmail.com>
2026-05-12 09:33:59 -0400

    Clamp Linux memory stats to size_t

--- src/eatmemory.linux.c
+++ src/eatmemory.linux.c
@@ -2,6 +2,11 @@
 #ifdef SYSMEM_MODE_LINUX
 #include <unistd.h>
 
+static size_t pages_to_bytes_clamped(long pages, long page_size) {
+    uint64_t bytes = (uint64_t)pages * (uint64_t)page_size;
+    return bytes > SIZE_MAX ? SIZE_MAX : (size_t)bytes;
+}
+
 void get_system_memory_stats(struct system_memory_stats* stats) {
     stats->supported = false;
 
@@ -11,8 +16,8 @@ void get_system_memory_stats(struct system_memory_stats* stats) {
     long free_pages = sysconf(_SC_AVPHYS_PAGES);
 
     if (pages > 0 && page_size > 0 && free_pages > 0) {
-        stats->total = (size_t)pages * (size_t)page_size;
-        stats->free = (size_t)free_pages * (size_t)page_size;
+        stats->total = pages_to_bytes_clamped(pages, page_size);
+        stats->free = pages_to_bytes_clamped(free_pages, page_size);
         stats->supported = true;
     }
 #endif
